ccf75e97613244ece573b0641dc7c8623ddb1684
IlyaGolub <47134681+IlyaGolub@users.noreply.github.com>
2021-01-18 17:58:22 +1000

    Add project files.

---
+++ GeometricFigure/Controllers/FigureController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GeometricFigure.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FigureController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<FigureController> _logger;
+
+        public FigureController(ILogger<FigureController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("figure")]
+        public IEnumerable<WeatherForecast> GetFigure()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("figure/{id}")]
+        public IEnumerable<WeatherForecast> GetFigureId()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
